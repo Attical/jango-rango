8f86582a1b3444e96bb0be88d0d8e4c63bf755c6
Attical <81836258+Attical@users.noreply.github.com>
2022-03-21 19:28:23 +0000

    chapter 5 finished

--- myproject/rango/admin.py
+++ myproject/rango/admin.py
@@ -1,3 +1,8 @@
+from gettext import Catalog
 from django.contrib import admin
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
 
 # Register your models here.
